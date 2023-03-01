50eb15d05483c3ac2edb046d5cc0446d0c5e24c2
moa3337 <moa-x@hotmail.com>
2023-03-01 16:28:24 +0100

    aggiunta axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+import charactersList from './charactersList.vue';
+
+export default {
+    components: { charactersList },
+};
+</script>
+
+<template>
+    <main class="container">
+        <charactersList />
+    </main>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/charactersList.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            characters: [],
+        };
+    },
+
+    created() {
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then((response) => {
+            console.log(response);
+            this.characters = response.data.data;
+        });
+    },
+};
+</script>
+
+<template>
+    <div class="row">
+        <div class="col" v-for="character in characters">
+            <img v-bind:src="character.image_url" alt="">
+            <h3>{{ character.name }}</h3>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
